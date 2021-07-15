4e9b5e7fc08096ae0830e4ee815d04d7fb3e48e5
noam_tzi <noam.tz99@gmail.com>
2021-07-15 16:18:09 +0300

    challenges and achievements creation

--- src/containers/mobile/Rules.jsx
+++ src/containers/mobile/Rules.jsx
@@ -4,9 +4,10 @@ import { BackgroundCircle } from "../../components/designSystem/mobileDS";
 import { MobileInfoBox } from "../../components/mobileComponents/mobileInfoBox/mobileInfoBox";
 import { Bottombar } from "../../components/mobileComponents/bottomBar/bottomBar";
 
-export default function MobileOverview() {
+export default function MobileRules() {
     return (
         <div>
+            Rules
             <div className="d-flex flex-column">
                 <BackgroundCircle src={Teal} alt="Upper background color" />
                 <MobileInfoBox subtitle="Challenge is on!" title="Surprise sugar attack-sauce" text="Find a sauce that contains more sugar than you thought (look at the ingridients!) and replace it #1" />
