47a3b4f5e97ae1d74bcff59eee88e6ba4d734787
Avigail <iggiebaru@gmail.com>
2021-07-17 21:16:33 +0300

    Initial leaderboard exists, currChallenge still in proccess

---
+++ src/components/mobileComponents/mobileLeaderboard/topThree.jsx
@@ -0,0 +1,3 @@
+import React, {useState, useEffect} from "react";
+import { useAuth } from "../../../context/AuthContext";
+import { StyledTitle, StyledText, LeaderboardDiv, LeaderboardContainer, LeaderBoardPhoto } from "../../designSystem/mobileDS";
