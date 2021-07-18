ebbfd158f7ba38c52f872f8a8772926f6ae9a5c5
Avigail <iggiebaru@gmail.com>
2021-07-18 13:13:47 +0300

    TopThree component done

--- src/containers/mobile/MobileOverview.jsx
+++ src/containers/mobile/MobileOverview.jsx
@@ -3,13 +3,14 @@ import Teal from "../../components/svgs/mobileBackgrounds/Teal.svg";
 import { BackgroundCircle } from "../../components/designSystem/mobileDS";
 import { MobileInfoBox } from "../../components/mobileComponents/mobileInfoBox/mobileInfoBox";
 import { MobileLeaderboard } from "../../components/mobileComponents/mobileLeaderboard/mobileLeaderboard";
+import { TopThree } from "../../components/mobileComponents/mobileLeaderboard/topThree";
 
 export default function MobileOverview() {
     return (
         <>
             <BackgroundCircle className="col 12" src={Teal} alt="Upper background color" />
             <div className="position-absolute">
-                <MobileInfoBox type="currChallenge" />
+                <TopThree />
             </div>
         </>
     );
