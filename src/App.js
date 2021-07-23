f040aaf3f528202000f4857ee09c91a448c9da7c
Avigail <iggiebaru@gmail.com>
2021-07-24 01:55:47 +0300

    404 page routing works! needs final desktop design

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 import React, { useState, useEffect } from "react";
-import { BrowserRouter as Router, Switch, Route, useHistory, useParams, Redirect } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  useHistory,
+  useParams,
+  Redirect,
+  useLocation,
+} from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
 import PageTemplate from "./containers/pages/pageTemplate";
 import Overview from "./containers/pages/overview";
@@ -14,9 +22,10 @@ import MobileChallenges from "./containers/mobile/MobileChallenges";
 import MobileAchievements from "./containers/mobile/MobileAchievements";
 import MobileRules from "./containers/mobile/Rules";
 // sign up/log in proccess
-import SignUpNG from "./containers/pages/auth/SignUpNG"
+import SignUpNG from "./containers/pages/auth/SignUpNG";
 import SignUpJG from "./containers/pages/auth/SignUpJG";
 import Login from "./containers/pages/auth/LogIn";
+import NotFoundPage from "./containers/pages/NotFoundPage";
 
 function getWindowDimensions() {
   const { innerWidth: width, innerHeight: height } = window;
@@ -44,71 +53,92 @@ function useWindowDimensions() {
 }
 
 function App() {
+  const ValidPaths = [
+    "/",
+    "/signup",
+    "/login",
+    "/signup/:groupId",
+    "/overview",
+    "/challenges",
+    "/achievements",
+    "/rulesOfGame",
+    "/styling",
+    "/progress",
+  ];
+
+  const currPath = window.location.pathname;
   var dimentions = useWindowDimensions();
 
+  if (ValidPaths.indexOf(currPath) > -1) {
+    if (dimentions.width < 500) {
+      // needs to be the mobile routing!!!
 
-  if (dimentions.width < 500) {
-    // needs to be the mobile routing!!!
-
-    return (
-      <Router>
-        <AuthProvider>
-          <Switch>
-          <Route exact path="/" component={MobileLanding} />
-            <Route path="/signup" exact component={SignUpNG} />
-            <Route path="/login" exact component={Login} />
-            <Route path="/signup/:groupId" component={SignUpJG}/>
-            <MobileHomeTemplate>
-              <Route
-                exact
-                mode="white"
-                path="/overview"
-                component={MobileOverview}
-              />
-              <Route
-                exact
-                path="/challenges"
-                component={MobileChallenges}
-                mode="black"
-              />
-              <Route
-                exact
-                path="/achievements"
-                component={MobileAchievements}
-                mode="white"
-              />
-              <Route
-                exact
-                path="/rulesOfGame"
-                component={MobileRules}
-                mode="black" />
-            </MobileHomeTemplate>
-          </Switch>
-        </AuthProvider>
-        {/* <Route path='/404' component={NotFoundPage} />
-        <Redirect from='*' exactto='/404' /> */}
-      </Router>
-    );
-    // end of mobile routing
+      return (
+        <Router>
+          <AuthProvider>
+            <Switch>
+              <Route exact path="/" component={MobileLanding} />
+              <Route path="/signup" exact component={SignUpNG} />
+              <Route path="/login" exact component={Login} />
+              <Route path="/signup/:groupId" component={SignUpJG} />
+              <MobileHomeTemplate>
+                <Route
+                  exact
+                  mode="white"
+                  path="/overview"
+                  component={MobileOverview}
+                />
+                <Route
+                  exact
+                  path="/challenges"
+                  component={MobileChallenges}
+                  mode="black"
+                />
+                <Route
+                  exact
+                  path="/achievements"
+                  component={MobileAchievements}
+                  mode="white"
+                />
+                <Route
+                  exact
+                  path="/rulesOfGame"
+                  component={MobileRules}
+                  mode="black"
+                />
+              </MobileHomeTemplate>
+            </Switch>
+          </AuthProvider>
+        </Router>
+      );
+      // end of mobile routing
+    } else {
+      return (
+        <Router>
+          <AuthProvider>
+            <Switch>
+              <Route path="/" exact component={HomePage} />
+              <Route path="/styling" component={StyleExamples} />
+              <Route path="/signup" exact component={SignUpNG} />
+              <Route path="/login" exact component={Login} />
+              <Route path="/signup/:groupId" component={SignUpJG} />
+              <PageTemplate>
+                <Route path="/overview" exact component={Overview} />
+                <Route path="/challenges" exact component={ChallengePage} />
+                <Route path="/progress" exact component={ProgressPage} />
+              </PageTemplate>
+            </Switch>
+          </AuthProvider>
+        </Router>
+      );
+    }
   } else {
     return (
       <Router>
-        <AuthProvider>
-          <Switch>
-          <Route path="/" exact component={HomePage} />
-          <Route path="/styling" component={StyleExamples} />
-          <Route path="/signup" exact component={SignUpNG} />
-          <Route path="/login" exact component={Login} />
-          <Route path="/signup/:groupId" component={SignUpJG}/>
-          <PageTemplate>
-            <Route path="/overview" exact component={Overview}/>
-            <Route path="/challenges" exact component={ChallengePage} />
-            <Route path="/progress" exact component={ProgressPage} />
-          </PageTemplate>
-          </Switch>
-        </AuthProvider>
+        <Switch>
+          <Route path="*" component={NotFoundPage} />
+        </Switch>
       </Router>
-
     );
   }
 }
